938ba0a020307a4e0602b6bdf150d0f628e506e4
Alexey Naydenov <alexey.naydenovREMOVETHIS@gmail.com>
2013-05-15 11:04:37 +0400

    Initial commit for equation calculator.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject eqalc "0.1.0-SNAPSHOT"
+  :description "Equation calculator"
+  :url "https://github.com/alexey-naydenov/eqalc"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main eqalc.core)
---
+++ test/eqalc/core_test.clj
@@ -0,0 +1,7 @@
+(ns eqalc.core-test
+  (:require [clojure.test :refer :all]
+            [eqalc.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

f16012740ccbdf2617ee4b9558ce1c348f783b02
Alexey Naydenov <alexey.naydenovREMOVETHIS@gmail.com>
2013-05-15 11:43:32 +0400

    Basic seesaw scafolding.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/alexey-naydenov/eqalc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [seesaw "1.4.3"]]
   :main eqalc.core)
--- src/eqalc/core.clj
+++ src/eqalc/core.clj
@@ -1,9 +1,14 @@
 (ns eqalc.core
-  (:gen-class))
+  (:gen-class)
+  (:use [seesaw core mig]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
+(defn -main [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (invoke-later
+   (-> (frame :title "Hello"
+              :content "Hello, Seesaw"
+              :width 800
+              :height 600
+              :on-close :exit)
+       show!)))

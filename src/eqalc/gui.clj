fa7fad1eaa4005fd5d1e2f4bd79af02aef7ccd38
Alexey Naydenov <alexey.naydenovREMOVETHIS@gmail.com>
2013-05-16 14:47:10 +0400

    Change structure to store edit elements.

--- src/eqalc/gui.clj
+++ src/eqalc/gui.clj
@@ -2,14 +2,30 @@
   (:use [seesaw core mig]))
 
 (defn equation->gui [{:keys [name unit]}]
-  [[name ""]
-   [(text :columns 10) ""]
-   [unit "left, wrap"]])
+  {:name-label [name ""]
+   :value-edit [(text :columns 10) ""]
+   :unit-label [unit "left, wrap"]})
+
+(defn guis->items [guis]
+  (apply concat (map (fn [m] [(m :name-label) (m :value-edit) (m :unit-label)])
+                     guis)))
 
 (defn equations->items [eqns]
   (apply concat (map equation->gui eqns)))
 
+(defn a-calculate [e] (alert "Calculate"))
+
+(defn a-exit [e] (dispose! e))
+
 (defn equations->panel [eqns]
+  (let [value-guis (map equation->gui eqns)
+        
+        calculate-button (button :text "Calculate"
+                                 :listen [:action a-calculate])
+        quit-button (button :text "Quit" 
+                            :listen [:action a-exit])]
   (mig-panel :constraints ["", "[right]"]
-             :items (concat (equations->items eqns)
-                            [[:separator "growx, span, wrap, gaptop 10"]])))
+             :items (concat (guis->items value-guis)
+                            [[:separator "growx, span, wrap, gaptop 10"]
+                             [calculate-button "span 2"]
+                             [quit-button ""]]))))

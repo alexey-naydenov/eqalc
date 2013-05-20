37d8379b2426f5d436e02d818f212b646bded90a
Alexey Naydenov <alexey.naydenovREMOVETHIS@gmail.com>
2013-05-20 10:38:17 +0400

    Create function for parsing prefixed numbers.

--- src/eqalc/prefix.clj
+++ src/eqalc/prefix.clj
@@ -1,4 +1,5 @@
 (ns eqalc.prefix
+  (:use [clojure set])
   (:require [clojure.math.numeric-tower :as math]))
 
 (defn redable-power
@@ -10,15 +11,24 @@
                  (recur (* val 1000.0) (- rpow 3))
                  rpow))))
 
-(def metric-prefixes {-12 "p" -9 "n" -6 "u" -3 "m" 0 ""
-                      3 "k" 6 "M" 9 "G" 12 "T"})
+(def power-prefix-map {-12 "p" -9 "n" -6 "u" -3 "m" 0 ""
+                       3 "k" 6 "M" 9 "G" 12 "T"})
+(def prefix-power-map (map-invert power-prefix-map))
 
 (defn power->prefix [rpow]
   ;; Find metric prefix for given redable power.
-  (metric-prefixes rpow))
+  (power-prefix-map rpow))
 
 (defn pformat [val]
   ;; Convert numeric value into a nice looking string with unit prefix.
   (let [dval (double val)
         rpow (redable-power dval)]
     (format "%.2f %s" (/ val (math/expt 10.0 rpow)) (power->prefix rpow))))
+
+(def prefixed-pattern #"\s*([+\-0-9.]+)\s*([pnumkMGT]?)\s*")
+
+(defn pscan [num-string]
+  (if-let [[whole-str num prefix] (re-matches prefixed-pattern num-string)]
+    (* (read-string num) (math/expt 10.0 (prefix-power-map prefix)))
+    (throw (NumberFormatException. 
+            (str  "Invalid prefix: " num-string)))))

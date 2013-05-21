4d4115d02b0f5b9cced61b8a27fcd3432d2744b1
Alexey Naydenov <alexey.naydenovREMOVETHIS@gmail.com>
2013-05-21 16:52:39 +0400

    Experimenting with calc grammar.

--- src/eqalc/calculator.clj
+++ src/eqalc/calculator.clj
@@ -9,20 +9,31 @@
 (defn calculate [vals eqns]
   (reduce update-values vals (filter (partial calculate-equation? vals) eqns)))
 
-(def arithmetic
-  (insta/parser
-   "expr = add-sub
-    <add-sub> = mul-div | add | sub
-    add = add-sub <'+'> mul-div
-    sub = add-sub <'-'> mul-div
-    <mul-div> = term | mul | div
-    mul = mul-div <'*'> term
-    div = mul-div <'/'> term
-    <term> = number | <'('> add-sub <')'>
-    number = #'[0-9]+'"))
+(def equation-grammar 
+  ["expr = add-sub"
+   "<add-sub> = mul-div | add | sub"
+   "add = add-sub <'+'> mul-div"
+   "sub = add-sub <'-'> mul-div"
+   "<mul-div> = term | mul | div"
+   "mul = mul-div <'*'> term"
+   "div = mul-div <'/'> term"
+   "function = name <'('> <sp*> arguments <sp*> <')'>"
+   "arguments = args"
+   "<args> = term | term <sp* ',' sp*> args | Epsilon"
+   "<term> = <sp*> name <sp*> | <sp*> number <sp*> | add-sub | <'('> add-sub <')'>| <sp*> function <sp*>"
+   "name = #'[a-zA-Z]+[a-zA-Z0-9_-]*'"
+   "number = #'[0-9]+'"
+   "sp = #'[\\s\\t]+'"])
+
+(def equation-parser (insta/parser 
+                      (apply str (interpose "\n" equation-grammar))))
 
 (def evaluation-transform {:add + :sub - :mul * :div /
                            :number read-string :expr identity})
 
+(defn equation->ast [equation]
+  (equation-parser equation))
+
 (defn evaluate-expression [expression-string]
-  (->> (arithmetic expression-string) (insta/transform evaluation-transform)))
+  (->> (equation-parser expression-string) 
+       (insta/transform evaluation-transform)))

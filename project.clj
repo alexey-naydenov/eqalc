a6058294609d45e092882ccf8ec0f09fd749352a
Alexey Naydenov <alexey.naydenovREMOVETHIS@gmail.com>
2013-05-21 08:19:47 +0400

    Add instaparse to the project.

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]
-                 [seesaw "1.4.3"]]
+                 [seesaw "1.4.3"]
+                 [instaparse "1.1.0"]]
   :main eqalc.core)
--- src/eqalc/calculator.clj
+++ src/eqalc/calculator.clj
@@ -1,4 +1,5 @@
-(ns eqalc.calculator)
+(ns eqalc.calculator
+  (:require [instaparse.core :as insta]))
 
 (defn update-values [vals {:keys [name fun]}]
   (conj vals {name ((eval fun) vals)}))
@@ -8,5 +9,20 @@
 (defn calculate [vals eqns]
   (reduce update-values vals (filter (partial calculate-equation? vals) eqns)))
 
+(def arithmetic
+  (insta/parser
+   "expr = add-sub
+    <add-sub> = mul-div | add | sub
+    add = add-sub <'+'> mul-div
+    sub = add-sub <'-'> mul-div
+    <mul-div> = term | mul | div
+    mul = mul-div <'*'> term
+    div = mul-div <'/'> term
+    <term> = number | <'('> add-sub <')'>
+    number = #'[0-9]+'"))
 
+(def evaluation-transform {:add + :sub - :mul * :div /
+                           :number read-string :expr identity})
 
+(defn evaluate-expression [expression-string]
+  (->> (arithmetic expression-string) (insta/transform evaluation-transform)))
